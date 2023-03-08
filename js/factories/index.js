707dfc774fcefe523611199388d82f5c9f79dff4
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2023-03-08 15:59:38 +0100

    style : maquette a jour

--- js/factories/index.js
+++ js/factories/index.js
@@ -1,7 +1,41 @@
-function mealFactory(data) {
-  const { name, ingredients, time, description } = data;
-
-  function getMealCardDOM() {
-    
+class RecipeCardFactory {
+  static create(recipe) {
+    const recipeCard = document.createElement('div');
+    recipeCard.classList.add('recipe-card');
+    recipeCard.innerHTML = `
+      <div class="recipe-header"></div>
+      <div class="recipe-information">
+      <h2 class="recipe-name">${recipe.name}</h2>
+      <p class="recipe-time"><img src="/images/time.png" class="time"> ${
+        recipe.time
+      } min</p>
+      <ul class="recipe-ingredients">
+        ${recipe.ingredients
+          .map(
+            (ingredient) =>
+              `<li>${ingredient.ingredient}: ${ingredient.quantity} ${
+                ingredient.unit || ''
+              }</li>`
+          )
+          .join('')}
+      </ul>
+      <p class="recipe-description">${recipe.description}</p>
+      </div>
+    `;
+    return recipeCard;
   }
+}
+
+const recipesContainer = document.querySelector('#section-meal');
+
+if (recipesContainer) {
+  // Générer les cartes de recettes avec le factory et les ajouter à la balise div
+  recipes.forEach((recipe) => {
+    const recipeCard = RecipeCardFactory.create(recipe);
+    recipesContainer.appendChild(recipeCard);
+  });
+} else {
+  console.error(
+    "La balise div avec l'ID 'section-meal' n'existe pas dans le document"
+  );
 }
--- js/pages/index.js
+++ js/pages/index.js
@@ -22,6 +22,25 @@ ingredientTitle.addEventListener('click', function () {
   searchInput.focus();
   searchInput.value = '';
   arrowIcon.classList.add('rotate180');
+
+  // afficher la liste des ingrédients dans la barre de recherche
+  let ingredientList = [];
+  let ingredientDiv = document.createElement('div');
+  ingredientDiv.classList.add('listBtn');
+  ingredientDiv.id = 'listIngredients';
+  searchBox.appendChild(ingredientDiv);
+
+  for (let recipe of recipes) {
+    for (let ingredient of recipe.ingredients) {
+      if (!ingredientList.includes(ingredient.ingredient.toLowerCase())) {
+        ingredientList.push(ingredient.ingredient.toLowerCase());
+        let p = document.createElement('p');
+        p.textContent = ingredient.ingredient;
+        ingredientDiv.appendChild(p);
+      }
+    }
+  }
+  searchBox.appendChild(ingredientDiv);
 });
 
 arrowIcon.addEventListener('click', function () {
@@ -31,11 +50,30 @@ arrowIcon.addEventListener('click', function () {
     searchInput.focus();
     searchInput.value = '';
     arrowIcon.classList.add('rotate180');
+
+    // afficher la liste des ingrédients dans la barre de recherche
+    let ingredientList = [];
+    for (let recipe of recipes) {
+      for (let ingredient of recipe.ingredients) {
+        if (!ingredientList.includes(ingredient.ingredient.toLowerCase())) {
+          ingredientList.push(ingredient.ingredient.toLowerCase());
+          let ingredientDiv = document.createElement('div');
+          ingredientDiv.textContent = ingredient.ingredient;
+          searchBox.appendChild(ingredientDiv);
+        }
+      }
+    }
   } else {
     ingredientTitle.style.display = 'flex';
     searchBox.style.display = 'none';
     searchInput.value = 'Rechercher un ingrédient';
     arrowIcon.classList.remove('rotate180');
+
+    // enlever la liste des ingrédients de la barre de recherche
+    let ingredientDivs = searchBox.querySelectorAll('div');
+    for (let div of ingredientDivs) {
+      searchBox.removeChild(div);
+    }
   }
 });
 
@@ -45,6 +83,12 @@ document.addEventListener('click', function (event) {
     searchBox.style.display = 'none';
     searchInput.value = 'Rechercher un ingrédient';
     arrowIcon.classList.remove('rotate180');
+
+    // enlever la liste des ingrédients de la barre de recherche
+    let ingredientDivs = searchBox.querySelectorAll('div');
+    for (let div of ingredientDivs) {
+      searchBox.removeChild(div);
+    }
   }
 });
 
@@ -71,6 +115,23 @@ applianceTitle.addEventListener('click', function () {
   applianceSearchInput.focus();
   applianceSearchInput.value = '';
   applianceArrowIcon.classList.add('rotate180');
+
+  // afficher la liste des appareils dans la barre de recherche
+  let applianceList = [];
+  let applianceDiv = document.createElement('div');
+  applianceDiv.classList.add('listBtn');
+  applianceDiv.id = 'listAppliances';
+  applianceSearchBox.appendChild(applianceDiv);
+
+  for (let recipe of recipes) {
+    if (!applianceList.includes(recipe.appliance.toLowerCase())) {
+      applianceList.push(recipe.appliance.toLowerCase());
+      let p = document.createElement('p');
+      p.textContent = recipe.appliance;
+      applianceDiv.appendChild(p);
+    }
+  }
+  applianceSearchBox.appendChild(applianceDiv);
 });
 
 applianceArrowIcon.addEventListener('click', function () {
@@ -80,11 +141,28 @@ applianceArrowIcon.addEventListener('click', function () {
     applianceSearchInput.focus();
     applianceSearchInput.value = '';
     applianceArrowIcon.classList.add('rotate180');
+
+    // afficher la liste des appareils dans la barre de recherche
+    let applianceList = [];
+    for (let recipe of recipes) {
+      if (!applianceList.includes(recipe.appliance.toLowerCase())) {
+        applianceList.push(recipe.appliance.toLowerCase());
+        let applianceDiv = document.createElement('div');
+        applianceDiv.textContent = recipe.appliance;
+        applianceSearchBox.appendChild(applianceDiv);
+      }
+    }
   } else {
     applianceTitle.style.display = 'flex';
     applianceSearchBox.style.display = 'none';
     applianceSearchInput.value = 'Rechercher un appareil';
     applianceArrowIcon.classList.remove('rotate180');
+
+    // enlever la liste des appareils de la barre de recherche
+    let applianceDivs = applianceSearchBox.querySelectorAll('div');
+    for (let div of applianceDivs) {
+      applianceSearchBox.removeChild(div);
+    }
   }
 });
 
@@ -94,6 +172,12 @@ document.addEventListener('click', function (event) {
     applianceSearchBox.style.display = 'none';
     applianceSearchInput.value = 'Rechercher un appareil';
     applianceArrowIcon.classList.remove('rotate180');
+
+    // enlever la liste des appareils de la barre de recherche
+    let applianceDivs = applianceSearchBox.querySelectorAll('div');
+    for (let div of applianceDivs) {
+      applianceSearchBox.removeChild(div);
+    }
   }
 });
 
@@ -106,7 +190,7 @@ let ustensilContainer = document.getElementById('ustensilContainer');
 let ustensilTitle = document.getElementById('ustensilTitle');
 let ustensilSearchBox = document.getElementById('ustensilSearchBox');
 let ustensilSearchInput = document.getElementById('ustensilSearch');
-let ustensilArrowIcon = document.getElementById('usentsilArrow');
+let ustensilArrowIcon = document.getElementById('ustensilArrow');
 
 ustensilSearchInput.addEventListener('click', function () {
   if (ustensilSearchInput.value === 'Rechercher un ustensile') {
@@ -120,6 +204,25 @@ ustensilTitle.addEventListener('click', function () {
   ustensilSearchInput.focus();
   ustensilSearchInput.value = '';
   ustensilArrowIcon.classList.add('rotate180');
+
+  // afficher la liste des ustensiles dans la barre de recherche
+  let ustensilList = [];
+  let ustensilDiv = document.createElement('div');
+  ustensilDiv.classList.add('listBtn');
+  ustensilDiv.id = 'listUstensils';
+  ustensilSearchBox.appendChild(ustensilDiv);
+
+  for (let recipe of recipes) {
+    for (let ustensil of recipe.ustensils) {
+      if (!ustensilList.includes(ustensil.toLowerCase())) {
+        ustensilList.push(ustensil.toLowerCase());
+        let p = document.createElement('p');
+        p.textContent = ustensil;
+        ustensilDiv.appendChild(p);
+      }
+    }
+  }
+  ustensilSearchBox.appendChild(ustensilDiv);
 });
 
 ustensilArrowIcon.addEventListener('click', function () {
@@ -129,11 +232,30 @@ ustensilArrowIcon.addEventListener('click', function () {
     ustensilSearchInput.focus();
     ustensilSearchInput.value = '';
     ustensilArrowIcon.classList.add('rotate180');
+
+    // afficher la liste des ustensiles dans la barre de recherche
+    let ustensilList = [];
+    for (let recipe of recipes) {
+      for (let ustensil of recipe.ustensils) {
+        if (!ustensilList.includes(ustensil.toLowerCase())) {
+          ustensilList.push(ustensil.toLowerCase());
+          let ustensilDiv = document.createElement('div');
+          ustensilDiv.textContent = ustensil;
+          ustensilSearchBox.appendChild(ustensilDiv);
+        }
+      }
+    }
   } else {
     ustensilTitle.style.display = 'flex';
     ustensilSearchBox.style.display = 'none';
     ustensilSearchInput.value = 'Rechercher un ustensile';
     ustensilArrowIcon.classList.remove('rotate180');
+
+    // enlever la liste des ustensiles de la barre de recherche
+    let ustensilDivs = ustensilSearchBox.querySelectorAll('div');
+    for (let div of ustensilDivs) {
+      ustensilSearchBox.removeChild(div);
+    }
   }
 });
 
@@ -143,5 +265,10 @@ document.addEventListener('click', function (event) {
     ustensilSearchBox.style.display = 'none';
     ustensilSearchInput.value = 'Rechercher un ustensile';
     ustensilArrowIcon.classList.remove('rotate180');
+    // enlever la liste des ustensiles de la barre de recherche
+    let ustensilDivs = ustensilSearchBox.querySelectorAll('div');
+    for (let div of ustensilDivs) {
+      ustensilSearchBox.removeChild(div);
+    }
   }
 });
